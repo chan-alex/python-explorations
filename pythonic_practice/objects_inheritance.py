0e15e9c8d367aef917a3a89798baa231d68542ea
alex chan <chan.k.leong@gmail.com>
2017-01-14 20:01:14 +0800

    Add some stuff to objects_inheritance.py

--- pythonic_practice/objects_inheritance.py
+++ pythonic_practice/objects_inheritance.py
@@ -26,3 +26,21 @@ i1 = ImaginaryNum(10, 10)
 print("{} + {}j ".format(i1.real, i1.imaginary))
 
 
+
+
+# It is possible to inherit from python bultins such the list typw
+class myList(list):
+    
+    def print_all(self):
+        for i in self:
+            print(i)
+
+l =myList()
+l.append(1)
+l.append(2)
+l.append(3)
+l.print_all()
+
+print(l[2])  # Because myList is a list, subscript works
+
+
